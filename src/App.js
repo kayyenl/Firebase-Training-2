6310a60ade4775343fcdd6fc3ad579a35d298add
kayyenl <yenyenlee9@gmail.com>
2022-08-21 01:45:55 +0800

    finally remembered the onchange and form tricks by Sonny

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import db from "./firebase";
 
 function App() {
   const [users, setUsers] = useState([])
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
   //establishing a connection to a specific collection
   const usersCollectionRef = collection(db, "users")
   const userCollectionRef = doc(db, "users", "pedoo")
@@ -24,8 +26,18 @@ function App() {
     getUsers()
   }, [])
 
+  async function createUser(e) {
+    e.preventDefault()
+    console.log(name, age)
+  }
+
   return (
     <div className="App">
+      <form>
+        <input placeholder="add ur name here..." type="text" value={name} onChange={(event) => setName(event.target.value)}></input>
+        <input placeholder="add ur age here..." type="number" value={age} onChange={(event) => setAge(event.target.value)}></input>
+        <button onClick={createUser}> Create a user! </button>
+      </form>
       {users.map((doc) => <>
         <h1> {doc.name} </h1>
         <p> {doc.id} </p>
